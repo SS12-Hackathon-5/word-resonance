087d3bb823e031b08e7565c267b617cef670b1d8
SengD <85476056+SengD@users.noreply.github.com>
2024-02-03 17:22:13 -0800

    Update Game.jsx

--- hangman/src/components/pages/Game.jsx
+++ hangman/src/components/pages/Game.jsx
@@ -4,35 +4,45 @@ import { Modal, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Link } from "react-router-dom";
+
+
 
 const GameComponent = () => {
-  const [show, setShow] = useState(false);
+  
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [showStart, setShowStart] = useState(false);
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+  const [showHelp, setShowHelp] = useState(false);
+
+
+
+  const handleCloseStart = () => setShowStart(false);
+  const handleCloseLeaderboard = () => setShowLeaderboard(false);
+  const handleCloseHelp = () => setShowHelp(false);
+
+  const handleShowStart = () => setShowStart(true);
+  const handleShowLeaderboard = () => setShowLeaderboard(true);
+  const handleShowHelp = () => setShowHelp(true);
 
   return (
     <div>
         <Container>
             <Row>
                 <Col>
-                <Button variant="dark" className="mega" onClick={handleShow}>
+                <Button variant="dark" className="mega" onClick={handleShowStart}>
                   Start
                 </Button>
 
-                <Modal show={show} onHide={handleClose} size="lg" centered>
+                <Modal show={showStart} onHide={handleCloseStart} size="lg" centered>
                   <Modal.Header closeButton>
                     <Modal.Title>Select Difficulty</Modal.Title>
                   </Modal.Header>
                   <Modal.Body>
-                    <Link to="/Hangman">
-                    <Button variant="dark" className="mega">Easy</Button>
-                    </Link>
+                    <Button variant="dark" className="mega" onClick={handleCloseStart}>Easy</Button>
                     <br/><br/>
-                    <Button variant="dark" className="mega" onClick={handleClose}>Normal</Button>
+                    <Button variant="dark" className="mega" onClick={handleCloseStart}>Normal</Button>
                     <br/><br/>
-                    <Button variant="dark" className="mega" onClick={handleClose}>Hard</Button>
+                    <Button variant="dark" className="mega" onClick={handleCloseStart}>Hard</Button>
                   </Modal.Body>
                 </Modal>
                 </Col>
@@ -43,11 +53,36 @@ const GameComponent = () => {
             </Row>
             <br/><br/><br/>
             <Row>
-            <Button className="mega" variant="dark">Leaderboard</Button>
+            <Button className="mega" variant="dark" onClick= {handleShowLeaderboard}>
+            Leaderboard</Button>
+            <Modal show={showLeaderboard} onHide={handleCloseLeaderboard} size="lg" centered>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Leaderboard</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                  1st 
+                  </Modal.Body>
+                  <Modal.Body>
+                  2nd 
+                  </Modal.Body>
+                  <Modal.Body>
+                  3rd 
+                  </Modal.Body>
+                </Modal>
             </Row>
             <br/><br/><br/>
             <Row>
-            <Button className="mega" variant="dark">Help</Button>
+            <Button className="mega" variant="dark" onClick={handleShowHelp}>
+              Help</Button>
+              <Modal show={showHelp} onHide={handleCloseHelp} size="lg" centered>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Help</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                  You guess the letters of a word to solve the word. The word is given by text to speech. You make your guess by talking into the microphone. You are allowed 6 wrong before you are out of the game. 
+
+                  </Modal.Body>
+                </Modal>
         </Row>
         </Container>
     </div>
